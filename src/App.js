9876c1ab6e0ba2736f50c78eb8ba4d2dc9462fc4
Cristian Navarrete <cristiannavarrete@outlook.com>
2021-04-18 17:58:38 -0400

    Agregar redux

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function Header() {
+    const nombre = useSelector((store => store.username));
+
     return (
-       <Navbar bg="dark" variant="dark">
+       <Navbar bg="dark" variant="dark" className="d-flex justify-content-between align-items-center">
             <Navbar.Brand href="#home">
-            
-            AppName
+            <div>INF236 Boilerplate</div>
+        
             </Navbar.Brand>
+            <div className="text-white"> Bienvenido {nombre}</div>
         </Navbar>
     )
 }
