78840608f2ccad8660783160ebbe2ef93252df89
Cristian Navarrete <cristiannavarrete@outlook.com>
2021-05-09 00:34:42 -0400

    Fix nombre de la constante de backend

--- src/repositories/user.js
+++ src/repositories/user.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
 const deleteUser = async (id) =>
-	axios.delete(`${process.env.BACKEND_URL}/users/${id}`);
+	axios.delete(`${process.env.REACT_APP_BACKEND_URL}/users/${id}`);
 
 const updateUser = async (id, data) =>
-	axios.patch(`${process.env.BACKEND_URL}/users/${id}`, data);
+	axios.patch(`${process.env.REACT_APP_BACKEND_URL}/users/${id}`, data);
 
 const createUser = async (data) =>
-	axios.post(`${process.env.BACKEND_URL}/users`, data);
+	axios.post(`${process.env.REACT_APP_BACKEND_URL}/users`, data);
 
-const getAllUsers = async () => axios.post(`${process.env.BACKEND_URL}/users`);
+const getAllUsers = async () => axios.post(`${process.env.REACT_APP_BACKEND_URL}/users`);
 
 const getUser = async (id) =>
-	axios.post(`${process.env.BACKEND_URL}/users/${id}`);
+	axios.post(`${process.env.REACT_APP_BACKEND_URL}/users/${id}`);
 
 // eslint-disable-nextline
 export { deleteUser, updateUser, createUser, getAllUsers, getUser };
